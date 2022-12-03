5e770a7c70750afc772c3568591e24fb1f8d890f
sapar6ek0v <razetka0309@gmail.com>
2022-12-03 13:29:27 +0300

    implemented Modal component

---
+++ src/components/Modal/index.tsx
@@ -0,0 +1,26 @@
+import { FC, ReactNode } from 'react';
+import styles from './styles.module.scss';
+
+type Props = {
+  isOpen: boolean;
+  onClose: () => void;
+  children: ReactNode;
+};
+
+const Modal: FC<Props> = ({ isOpen, onClose, children }) => {
+
+  return (
+    <div
+      onClick={onClose}
+      className={isOpen ? styles.modal_wrapper : `${styles.modal_wrapper} ${styles.out}`}
+    >
+      <div className={styles.modal_background}>
+        <div onClick={(event) => event.stopPropagation()} className={styles.modal_content}>
+          {children}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
