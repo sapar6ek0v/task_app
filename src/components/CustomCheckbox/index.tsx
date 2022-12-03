8f027c0707f7cfe7d18ee8f2093d53b341054186
sapar6ek0v <razetka0309@gmail.com>
2022-12-03 13:30:08 +0300

    implemented Checkbox component

---
+++ src/components/CustomCheckbox/index.tsx
@@ -0,0 +1,26 @@
+import { FC } from 'react';
+import styles from './styles.module.scss';
+
+type Props = {
+  isCompleted: boolean;
+  toggle: () => void;
+};
+
+const CustomCheckbox: FC<Props> = ({ isCompleted, toggle }) => {
+
+  return (
+    <label className={styles.checkbox_label}>
+      <input
+        checked={isCompleted}
+        onChange={toggle}
+        type="checkbox"
+        className={styles.checkbox_input}
+      />
+      <div
+        className={isCompleted ? `${styles.checkbox_div} ${styles.checked}` : styles.checkbox_div}
+      />
+    </label>
+  );
+};
+
+export default CustomCheckbox;
